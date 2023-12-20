f7f2dc13ccd75388fd362210b469c56603d6db8a
Little Squiddie <missshru2005@gmail.com>
2023-12-20 14:52:02 -0500

    name has been changed

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 let name = "Squiddie"
-let Accno = 15246887
+const Accno = 15246887
 let Password = "SQUIDDIE@123"
+// Accno = 123465789
+name = "Laudu"
 console.log(name);
 console.log(Accno);
 console.log(Password);
