29e1aaba2d3db1c3d72b05a47542714dd1e2ebcf
Little Squiddie <missshru2005@gmail.com>
2023-12-20 15:11:02 -0500

    Datatypes are present

---
+++ Datatypes.js
@@ -0,0 +1,17 @@
+console.log("Aadhaar Card Informations:")
+const aadharno = 417361449375
+let name = "Shrutishree Sasmal"
+let state = "Odisha"
+let fana ="Manas Ranjan Sasmal"
+let mana = "Tuni Devi"
+let phoneno= null
+let sex= "Female"
+let telegraphno
+console.log("Aadhar Number: "+aadharno);
+console.log("Name: "+name);
+console.log("State: "+state);
+console.log("Father's Name: "+fana);
+console.log("Mother's Name: "+mana);
+console.log("Phone Number: "+phoneno);
+console.log("Sex: "+sex);
+console.log("Telegraph Number: "+telegraphno);
--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 let name = "Squiddie"
 const Accno = 15246887
 let Password = "SQUIDDIE@123"
+let state = null 
 // Accno = 123465789
 name = "Little Squiddie"
 console.log(name);
 console.log(Accno);
 console.log(Password);
+console.log(typeof state);
+console.log(typeof Password);
